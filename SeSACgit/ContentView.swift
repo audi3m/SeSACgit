5c87124be7f96452b5d1e15fc1031ade98300859
ray <audi3m@naver.com>
2024-10-08 14:16:47 +0900

    First Task

---
+++ SeSACgit/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SeSACgit
+//
+//  Created by J Oh on 10/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+
